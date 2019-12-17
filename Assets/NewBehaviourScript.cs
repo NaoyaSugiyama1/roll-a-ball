2f8b884d979e5d731d7f558035cd64bebd8e6275
NaoyaSugiyama1 <21911198ns@tama.ac.jp>
2019-12-17 13:26:50 +0900

    roll a ballを加えた

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Stoker : MonoBehaviour
+{
+    public GameObject target;   // 追跡するターゲット
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
---
+++ Assets/aaa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Rotater : MonoBehaviour
+{
+    public float RotateSpeed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Rotate(new Vector3(0,90,0) * Time.deltaTime,
+			Space.World);
+    }
+}
